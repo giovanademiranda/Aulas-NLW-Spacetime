e286ccaab39780898efa46cb7316b406d67c7790
Giovana de Miranda <giovana.miranda@bitize.com.br>
2023-05-24 17:02:33 -0300

    cadastro do usuario

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
+import { prisma } from '../lib/prisma'
 
 export async function authRoutes(app: FastifyInstance) {
   app.post('/register', async (request) => {
@@ -8,10 +9,8 @@ export async function authRoutes(app: FastifyInstance) {
       code: z.string(),
     })
     const { code } = bodySchema.parse(request.body)
-    let acessTokenResponse = null
-
-    acessTokenResponse = await axios.post(
-      'https://github.com/login/oauth/acess_token',
+    const accessTokenResponse = await axios.post(
+      'https://github.com/login/oauth/access_token',
       null,
       {
         params: {
@@ -24,19 +23,51 @@ export async function authRoutes(app: FastifyInstance) {
         },
       }
     )
-    console.log(acessTokenResponse)
 
-    const { acess_token } = acessTokenResponse.data
+    const { access_token } = accessTokenResponse.data
 
     const userResponse = await axios.get('https://api.github.com/user', {
       headers: {
-        Authorization: `Bearer ${acess_token}`,
+        Authorization: `Bearer ${access_token}`,
       },
     })
-    const user = userResponse.data
+    const userSchema = z.object({
+      id: z.number(),
+      login: z.string(),
+      name: z.string(),
+      avatar_url: z.string().url(),
+    })
+
+    const userInfo = userSchema.parse(userResponse.data)
+
+    let user = await prisma.user.findUnique({
+      where: {
+        githubId: userInfo.id,
+      },
+    })
+    if (!user) {
+      user = await prisma.user.create({
+        data: {
+          githubId: userInfo.id,
+          login: userInfo.login,
+          name: userInfo.name,
+          avatarUrl: userInfo.avatar_url,
+        },
+      })
+    }
+    const token = app.jwt.sign(
+      {
+        name: user.name,
+        avatarUrl: user.avatarUrl,
+      },
+      {
+        sub: user.id,
+        expiresIn: '30 days',
+      }
+    )
 
     return {
-      user,
+      token,
     }
   })
 }
--- server/src/routes/server.ts
+++ server/src/routes/server.ts
@@ -1,13 +1,19 @@
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import 'dotenv/config'
 import fastify from 'fastify'
 import { authRoutes } from './auth'
 import { memoriesRoutes } from './memories'
 
 const app = fastify()
+
 app.register(cors, {
   origin: true,
 })
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
 app.register(authRoutes)
 app.register(memoriesRoutes)
 
