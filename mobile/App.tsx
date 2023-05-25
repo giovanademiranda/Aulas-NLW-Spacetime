40eaf4e9b9b9ad0db20b9a6e9348ac1a1899607e
Giovana de Miranda <giovana.miranda@bitize.com.br>
2023-05-25 12:10:45 -0300

    token de cadastro do usuario

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,8 +5,10 @@ import {
   useFonts,
 } from "@expo-google-fonts/roboto";
 
+import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
 import { StatusBar } from "expo-status-bar";
 import { styled } from "nativewind";
+import React, { useEffect } from "react";
 import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
 import blurBg from "./src/assets/bg-blur.png";
 import NLWLogo from "./src/assets/nlw-spacetime-logo.svg";
@@ -14,12 +16,40 @@ import Stripes from "./src/assets/stripes.svg";
 
 const StyledStripes = styled(Stripes);
 
+const discovery = {
+  authorizationEndpoint: "https://github.com/login/oauth/authorize",
+  tokenEndpoint: "https://github.com/login/oauth/access_token",
+  revocationEndpoint:
+    "https://github.com/settings/connections/applications/54145966a9e80ff6a1c9>",
+};
+
 export default function App() {
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   });
+  const [request, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: "54145966a9e80ff6a1c9",
+      scopes: ["identity"],
+      redirectUri: makeRedirectUri({
+        scheme: "nlwspacetime",
+      }),
+    },
+    discovery
+  );
+
+  useEffect(() => {
+    // console.log(
+    //   makeRedirectUri({
+    //     scheme: "nlwspacetime",
+    //   })
+    // );
+    if (response?.type === "success") {
+      const { code } = response.params;
+    }
+  }, [response]);
 
   if (!hasLoadedFonts) {
     return null;
@@ -47,6 +77,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGithub()}
         >
           <Text className=" font-alt text-sm uppercase text-black">
             Cadastrar lembrança
